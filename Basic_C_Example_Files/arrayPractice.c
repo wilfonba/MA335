ee3a97f9a3e15377b7e388432659d02a9f9ee70b
wilfonba <wilfonba@rose-hulman.edu>
2021-03-16 15:04:17 -0400

    Finished up arrayPractice

--- Basic_C_Example_Files/arrayPractice.c
+++ Basic_C_Example_Files/arrayPractice.c
@@ -4,20 +4,35 @@
 
 int main(int argc, char **argv)
 {
+    // Determine number of numbers to enter
     char line[1000] = {};
     int N = 0;
     printf("Please enter an integer N to define the array size: ");
     fgets(line,1000,stdin);
     sscanf(line,"%d",&N);
+
+    // Data entry
+    double numbs[N];
+    printf("Please enter %2d numbers.\n",N);
+    for (int i=0;i<N;i++)
+    {
+        printf("Enter a number: ");
+        fgets(line,1000,stdin);
+        sscanf(line,"%lf",&numbs[i]);
+    }
+
+    // Calculate and output results
     double A[N];
     double B[N];
     double C[N];
 
-    for(int i = 0;i <= N;i++)
+    printf("  entry |   A[i]  |   B[i]  |   C[i]  \n");
+    for(int i = 0;i < N;i++)
     {
-        A[i] = sin(i);
-        B[i] = cos(i);
+        A[i] = sin(numbs[i]);
+        B[i] = cos(numbs[i]);
         C[i] = pow(A[i],2) + pow(B[i],2);
+        printf("%+3.4lf | %+1.4lf | %+1.4lf | %+1.4lf\n",numbs[i],A[i],B[i],C[i]);
     }
 
     return 0;
