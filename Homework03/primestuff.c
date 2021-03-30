14964ba7305c46f94cc6d0a3e191afe676a90585
wilfonba <wilfonba@rose-hulman.edu>
2021-03-30 13:28:15 -0400

    began primestuff.c

---
+++ Homework03/primestuff.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int isprime(int N)
+{
+
+    return 1;
+}
+
+int isMersenne(int N)
+{
+
+    return 1;
+}
+
+int main(int *argv, char **argc)
+{
+
+    return 0;
+}
