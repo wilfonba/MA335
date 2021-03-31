7564a22d5b5ce534cf5c7adf829ccd7b2f1433be
wilfonba <wilfonba@rose-hulman.edu>
2021-03-30 21:51:38 -0400

    finished prime stuff

--- Homework03/primestuff.c
+++ Homework03/primestuff.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-#include <strings.h>
+#include <string.h>
 
 int isprime(int N)
 {
@@ -57,9 +57,76 @@ void prime_info(int* ns, int l, int* num_prime, int* primes, int* num_mersenne, 
     }
 }
 
-
-int main(int argv, char **argc)
+void printArray(int *ar, int l)
 {
-    
+    printf("[");
+    if (l != 0)
+    {
+        for(int iii = 0;iii < l - 1;iii++)
+        {
+            printf("%d,",*(ar+iii));
+        }
+        printf("%d]\n",*(ar+l-1));
+        return;
+    }
+    printf("]\n");
+}
+
+void printPrimeInfo(int* ns, int l, int* num_prime, int* primes, int* num_mersenne, int* mersennes)
+{
+    printf("Consider the array ");
+    printArray(ns,l);
+    printf("There are %d prime elements, they are ",*num_prime);
+    printArray(primes,*num_prime);
+    printf("There are %d Mersenne prime elements, they are ",*num_mersenne);
+    printArray(mersennes,*num_mersenne);
+}
+
+int main(int argc, char **argv)
+{
+    if (argc < 3)
+    {
+        printf("See usage information\n");
+        return 0;
+    }
+    if (strcmp(argv[1],"--list") == 0)
+    {
+        int N = argc - 2;
+        int *primes=(int*)malloc(N*sizeof(int));
+        int *mersennes=(int*)malloc(N*sizeof(int));
+        int num_prime = 0;
+        int num_mersenne = 0;
+        int *numbers=(int*)malloc(N*sizeof(int));
+        for(int iii=0;iii < N;iii++)
+        {
+            numbers[iii] = atoi(argv[iii+2]);
+        }
+        prime_info(numbers,N,&num_prime,primes,&num_mersenne,mersennes);
+        printPrimeInfo(numbers,N,&num_prime,primes,&num_mersenne,mersennes);
+    }
+    else if (strcmp(argv[1],"--range") == 0)
+    {
+        if (argc < 4)
+        {
+            printf("See usage information\n");
+            return 0;
+        }
+        int min = atoi(argv[2]);
+        int max = atoi(argv[3]);
+        int N = max - min + 1;
+        int *numbers=(int*)malloc(N*sizeof(int));
+        int *primes=(int*)malloc(N*sizeof(int));
+        int *mersennes=(int*)malloc(N*sizeof(int));
+        int num_prime = 0;
+        int num_mersenne = 0;
+        int itr = 0;
+        for(int iii = min;iii <= max;iii++)
+        {
+            numbers[itr] = iii;
+            itr++;
+        }
+        prime_info(numbers,N,&num_prime,primes,&num_mersenne,mersennes);
+        printPrimeInfo(numbers,N,&num_prime,primes,&num_mersenne,mersennes);
+    }
     return 0;
 }
