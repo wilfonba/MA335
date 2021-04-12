1942c07fbc62377d9315d493b8022e0e8292b04e
wilfonba <wilfonba@rose-hulman.edu>
2021-04-12 16:57:04 -0400

    Worked on palindrom.c

--- Projects/Project01/palindrome.c
+++ Projects/Project01/palindrome.c
@@ -7,11 +7,17 @@
 
 #include "dict.c"
 
+#define debug
 
 typedef struct{
   int numwords;
   char* dictfile;
 }options;
+
+typedef struct{
+  int lowerBound;
+  int upperBound;
+}bounds;
 
 void print_options(options* opt) {
   printf("numwords: %d\n",opt->numwords);
@@ -64,6 +70,64 @@ int ispalindrome(char* word) {
   return answer;
     
 }
+
+void root_stuff(dictionary* D,int* size,options* opt) {
+  // allocate arrays for assignment steps
+  bounds* procBounds = malloc((*size)*sizeof(bounds));
+  int* procN = malloc((*size)*sizeof(int));
+
+  // Assign "how many words each process will check"
+  int i;
+  for (i = 0;i < *size;i++) {
+    procN[i] = D->size/(*size);
+    if (i < (D->size % *size)) {
+      procN[i]++;
+    }
+  }
+  // Assign upper and lower bounds to send to each processor
+  procBounds[0].lowerBound = 0;
+  procBounds[0].upperBound = procN[0] - 1;
+  for (i = 1;i < *size;i++) {
+    procBounds[i].lowerBound = procBounds[i-1].upperBound + 1;
+    procBounds[i].upperBound = procBounds[i].lowerBound + procN[i] - 1;
+  }
+
+  #ifdef debug
+    printf("The dictionary has %d words \n",D->size);
+    for (i = 0;i < *size;i++) {
+      printf("Process %d checking words [%d, %d] for a total of %d words \n",i,procBounds[i].lowerBound,procBounds[i].upperBound,
+        procBounds[i].upperBound - procBounds[i].lowerBound + 1);
+    }
+  #endif
+
+  // Get the upper and lower bounds for the root process
+  int myLB = procBounds[0].lowerBound;
+  int myUB = procBounds[0].upperBound;
+  
+  #ifdef debug
+    printf("I am process 0 checking the range [%d, %d]\n",myLB,myUB);
+  #endif
+
+  // Send the ranges to each worker process
+  int buffer[2];
+  for (i = 1;i < *size;i++) {
+    buffer[0] = procBounds[i].lowerBound;
+    buffer[1] = procBounds[i].upperBound;
+    MPI_Send(buffer,2,MPI_INT,i,0,MPI_COMM_WORLD);
+  }
+}
+
+void worker_stuff(int* rank) {
+  int buffer[2];
+  MPI_Status status;
+  MPI_Recv(buffer,2,MPI_INT,0,0,MPI_COMM_WORLD,&status);
+  int myLB = buffer[0];
+  int myUB = buffer[1];
+
+  #ifdef debug
+    printf("I am process %d checking the range [%d, %d]\n",*rank,myLB,myUB);
+  #endif
+}
   
 void main(int argc, char** argv) {
   MPI_Init(&argc,&argv);
@@ -83,22 +147,19 @@ void main(int argc, char** argv) {
   dict_open(opt.dictfile,&D);
 
   //Now, D.data[i] is a string with the i'th word in the dictionary in it. 
-  int i;
-  int j;
-  int k;
-  // initialize array of truples
-  int N = pow(D.size,opt.numwords);
-  int** truples = (int**)malloc(N*sizeof(int*));
-  for(i = 0;i < N;i++) {
-    truples[i] = malloc(3*sizeof(int));
+  if (rank == 0) {
+    root_stuff(&D,&size,&opt);
   }
-  for(i = 0;i < N;i++) {
-    for (j = 0;j < 3;j++) {
-      
+  else {
+    worker_stuff(&rank);
+  }
+
+  /*if (rank == 0) {
+    int** truples = (int**)malloc(N*sizeof(int*));
+    for(i = 0;i < N;i++) {
+      truples[i] = malloc(3*sizeof(int));
     }
-  }
-
-
+  }*/
   MPI_Finalize();
   exit(MPI_SUCCESS);
 }
