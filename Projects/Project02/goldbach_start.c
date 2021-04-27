4cd5870ae643e9195e6c9313e59d427961da80e7
wilfonba <wilfonba@rose-hulman.edu>
2021-04-27 10:13:13 -0400

    started project2

--- Projects/Project02/goldbach_start.c
+++ Projects/Project02/goldbach_start.c
@@ -7,6 +7,19 @@
 
 #include "goldbach_helpers.c"
 
+
+  /*TAG  -  USE
+  0   -  Sending a task to a worker node.
+  1   -  Sending the first part (ID and length) from a worker to the root.
+  2   -  Sending the last part (list of primes) for a result from a worker to
+          the root. 
+  3   -  Root sending a quit message to a worker.*/
+enum messageTags {
+  SEND_TASK = 0,
+  REQUEST_TASK = 1,
+  TAG_QUIT = 2,
+};
+
 int goldbach_partition_calls=0;
 
 //returns 1 if n is prime, 0 otherwise
@@ -23,8 +36,6 @@ int isprime(int n){
   }
   return 1;
 }
-
-
 
 //Attempts to calculate a Golbach partition of n.
 //Returns a Goldbach partition that includes the smallest possible prime.
@@ -58,21 +69,113 @@ int min(int a, int b){
   return a<b ? a:b;
 }
 
+int my_rank() {
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+  return rank;
+}
+
+void sendTask(int* lastTask,int nData, options* opt,int dest) {
+  int *sendBuff = malloc(nData*sizeof(int));
+  int i;
+  for(i = 0;i < nData;i++) {
+    sendBuff[i] = opt->to_partition[lastTask[1] + i];
+  }
+  MPI_Send(sendBuff,nData,MPI_INT,dest,SEND_TASK,MPI_COMM_WORLD);
+  free(sendBuff);
+}
+
+void sendQuit(int rank) {
+  MPI_Send(NULL,0,MPI_INT,rank,TAG_QUIT,MPI_COMM_WORLD);
+}
+
+void rootStuff(options* opt){
+  int rank = my_rank();
+  int numProcs;
+  MPI_Comm_size(MPI_COMM_WORLD,&numProcs);
+  // Determine number of tasks
+  int numTasks = opt->num_to_partition/opt->chunksize;
+  if (opt->num_to_partition % opt->chunksize != 0) {
+    numTasks++;
+  }
+  // Keeping track of tasks assigned and completed as well as describing them
+  // with as (id, lower index, upper index)
+  int tasksAssigned = 0;
+  int tasksCompleted = 0;
+  int lastTask[3] = {-1,1,1};
+  // Assign each worker an initial task unless there are none left to assign
+  int i = 0;
+  for(i = 1;i < numProcs;i++) {
+    if (tasksAssigned < numTasks){ 
+      lastTask[0]++;
+      lastTask[1] = lastTask[2] + 1;
+      lastTask[2] = min(lastTask[1]+opt->chunksize-1, opt->num_to_partition);
+      int nData = lastTask[2] - lastTask[1] + 1;
+      sendTask(lastTask,nData,opt,i);
+      tasksAssigned++;
+    }
+    else {
+      sendQuit(i);
+    }
+  }
+  // Allocate remaining tasks
+  while(tasksCompleted < numTasks) {
+    MPI_Status status;
+    // Receive request for new task
+    int buff;
+    MPI_Recv(&buff,1,MPI_INT,MPI_ANY_SOURCE,REQUEST_TASK,MPI_COMM_WORLD,&status);
+    int workerID = status.MPI_SOURCE;
+    tasksCompleted++;
+    // Assign requesting core a new task if there exists remaining tasks
+    if (tasksAssigned < numTasks) {
+      lastTask[0]++;
+      lastTask[1] = lastTask[2] + 1;
+      lastTask[2] = min(lastTask[1]+opt->chunksize-1, opt->num_to_partition);
+      int nData = lastTask[2] - lastTask[1] + 1;
+      sendTask(lastTask,nData,opt,i);
+      tasksAssigned++;
+    }
+    // Send quite message if no tasks remain
+    else {
+      sendQuit(workerID);
+    }
+  }
+}
+
+void workerStuff(options* opt){
+  int rank = my_rank();
+  MPI_Status status;
+  int* recvBuff = malloc(opt->chunksize*sizeof(int));
+  while (1) {
+    MPI_Recv(recvBuff,opt->chunksize,MPI_INT,0,SEND_TASK,MPI_COMM_WORLD,&status);
+    int nRecv = status.count;
+    int i;
+    int a;
+    int b;
+    int isGoldbach;
+    for (i = 0;i < nRecv;i++) {
+      isGoldback = goldbach_partition(recvBuff[i],&a,&b);
+      if 
+    }
+  }
+  free(recvBuff);
+}
 
 int main(int argc, char** argv){
-
   MPI_Init(&argc,&argv);
-
   
-  int rank;
+  int rank = my_rank();
   int size;
-  MPI_Comm_rank(MPI_COMM_WORLD,&rank);
   MPI_Comm_size(MPI_COMM_WORLD,&size);
   
   options opt;
   parse_options(argc,argv,&opt);
   if (rank==0){
     print_options(&opt);
+    rootStuff(&opt);
+  }
+  else {
+    workerStuff(&opt);
   }
 
   //The command line options have now been parsed properly.
@@ -83,8 +186,6 @@ int main(int argc, char** argv){
   //opt.num_to_partition -- the length of the list above
   //opt.chunksize -- the control for the "chunksize" of a work assignment.  If the chunksize is n, then processors get assigned to partition n consecutive numbers from the to_partition list as part of 1 assignement. 
   
-  
   MPI_Finalize();
-
-
+  return 0;
 }
