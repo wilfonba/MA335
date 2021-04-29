78360e063c068e3ff97108d7b6146fb9d64cf848
wilfonba <wilfonba@rose-hulman.edu>
2021-04-29 14:47:05 -0400

    incorrect my_gather worked on

--- Lectures/GatherImplementation/my_gather.c
+++ Lectures/GatherImplementation/my_gather.c
@@ -1,6 +1,7 @@
 #include <mpi.h>
 #include <stdio.h>
 #include "arr2str.c"
+#include <math.h>
 
 void my_gather(int* sendbuf, int len, int* recvbuf, int root) {
 	int rank;
@@ -8,6 +9,8 @@ void my_gather(int* sendbuf, int len, int* recvbuf, int root) {
 
 	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 	MPI_Comm_size(MPI_COMM_WORLD,&size);
+
+	int myRank = rank;
 
 	//Let's assume that the root is rank 0, and abort otherwise. 
 	if (root!=0){
@@ -27,19 +30,26 @@ void my_gather(int* sendbuf, int len, int* recvbuf, int root) {
 
 	int* tempBuff = malloc(len*sizeof(int));
 	while (level < size/2) {
-		if (rank % level*2 == 0) { 
-			MPI_Recv(tempBuff,len*level,MPI_INT,rank+level,0,MPI_COMM_WORLD,NULL);
-			recvBuff = realloc(recvBuff,2*sizeof(*recvBuff));
-			memcpy(recvBuff,tempBuff,sizeof(*tempBuff));
-			if (rank % level*4 != 0){
-				sendBuff = realloc(sendBuff,2*sizeof(*sendBuff));
-				memcpy(sendBuff,tempBuff,sizeof(sendBuff));
-				tempBuff = realloc(tempBuff,2*sizeof(*tempBuff));	
-			}
+		int N = (int) len*pow(2,level-1); 
+		if (myRank % 2 == 0) {
+			/*tempBuff = realloc(tempBuff,N*sizeof(int));
+			MPI_Recv(tempBuff,N,MPI_INT,0,myRank + 1,MPI_COMM_WORLD,NULL);
+			printf("Rank %d received [%d,%d,%d,%d]\n",myRank,tempBuff[0],tempBuff[1],tempBuff[2],tempBuff[3]);
+			fflush(stdout);
+			memcpy(recvBuff+N,tempBuff,len*pow(2,level-1)*sizeof(int));
+			myRank = myRank/2;
+			if (myRank % 2 != 0) {
+				tempBuff = realloc(tempBuff,2*N*sizeof(int));
+				memcpy(sendBuff,recvBuff,2*N*sizeof(int));
+			}*/
 		}
 		else {
-			MPI_Send(sendBuff,len*level,MPI_INT,rank,0,MPI_COMM_WORLD);
+			printf("N = %d: Rank %d sending [%d,%d,%d,%d]\n",N,myRank,sendBuff[0],sendBuff[1],sendBuff[2],sendBuff[3]);
+			fflush(stdout);
+			MPI_Send(sendBuff,N,MPI_INT,0,myRank - 1,MPI_COMM_WORLD);
+			myRank = myRank/2;
 		}
-		level*=2;
+
+		level++;
 	}
 }
